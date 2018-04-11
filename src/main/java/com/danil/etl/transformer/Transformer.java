9de0bac637597dca0fc947ecabbe7456ae292d86
danil <gerasimenkodanil@gmail.com>
2018-04-11 03:31:57 +0300

    ETL-11 removed debug changes

--- src/main/java/com/danil/etl/transformer/Transformer.java
+++ src/main/java/com/danil/etl/transformer/Transformer.java
@@ -53,7 +53,7 @@ public class Transformer {
         System.out.println("\rTransformation....Done");
     }
 
-    public void extractAndTransform(AtomicInteger chunkWithDuplicates) {
+    public void extractAndTransform(AtomicInteger chunkWithDuplicatesCounter) {
 
         final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(taskQueueSize);
         final ExecutorService executor = new ThreadPoolExecutor(1, poolSize, 0L, TimeUnit.MILLISECONDS, queue);
@@ -65,16 +65,7 @@ public class Transformer {
             prevRecordId = 0l;
         } else {
             System.out.println("WARN. Transformation resumed.");
-
-            for(TaskInfo taskInfo : currentProcessingState) {
-                List<Flight> chunk = null;
-                if (TransformTaskStatus.TRANSFORM.equals(taskInfo.getTaskStage())) {
-                    chunk = flightDao.getNextChunk(taskInfo.getStartIndex(), chunkSize);
-                    taskInfo.setTaskStage(TransformTaskStatus.EXTRACT);
-                }
-                executor.submit(new ScrapperTask(flightDao, taskInfoDao, chunk, taskInfo, totalHandledRecords, chunkWithDuplicates, taskTime));
-            }
-
+            resumeTasks(executor, currentProcessingState, chunkWithDuplicatesCounter);
             prevRecordId = currentProcessingState.get(currentProcessingState.size() - 1).getEndIndex();
         }
 
@@ -84,64 +75,10 @@ public class Transformer {
             final long chunkAmount = totalRecordsSize / chunkSize;
             String progressAnimation = "|/-\\";
             int animationCounter = 0;
-       //     while (tmpChunkSize > 0) {
-                Long startIndex = chunk.get(tmpChunkSize - 1).getId();
-                executor.submit(new ScrapperTask(flightDao, taskInfoDao, chunk, null, totalHandledRecords, chunkWithDuplicates, taskTime));
-                chunk = flightDao.getNextChunk(startIndex, chunkSize);
-                tmpChunkSize = chunk.size();
-
-                while (queue.size() == taskQueueSize) {
-                    Thread.sleep(ESTIMATED_TIME_TO_COMPLETE_ONE_TASK_MILIS);
-                }
-                if (tmpChunkSize > 0) {
-                    long timeEstimation = chunkAmount * taskTime.get();
-
-                    final String estimationMessage = String.format("Estimated time: %02d min",
-                            TimeUnit.MILLISECONDS.toMinutes(timeEstimation));
-
-                    System.out.print("\r" + estimationMessage + " Transformation..." + progressAnimation.charAt(animationCounter % progressAnimation.length()));
-                    animationCounter++;
-                }
-          //  }
-
-        } catch (InterruptedException ex) {
-            System.out.println("Someone tries to stop me => " + ex.getMessage());
-        }
-        finally {
-            executor.shutdown();
-        }
-        boolean done = false;
-        try {
-            done = executor.awaitTermination((poolSize + taskQueueSize) * ESTIMATED_TIME_TO_COMPLETE_ONE_TASK_MILIS, TimeUnit.MINUTES);
-        } catch (InterruptedException ex) {
-            System.out.println("Await termination Interrupted => " + ex.getMessage());
-            executor.shutdownNow();
-        }
-        if (!done) {
-            System.out.println("Application stopped by timeout. Some tasks stay inProgress. Exit.");
-        }
-    }
-
-
-
-
-
-    public void extractAndTransform1(TaskInfo taskInfo, Long prevRecordId, AtomicInteger counter) {
-
-        final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(taskQueueSize);
-        final ExecutorService executor = new ThreadPoolExecutor(1, poolSize, 0L, TimeUnit.MILLISECONDS, queue);
-
-        try {
-            List<Flight> chunk = flightDao.getNextChunk(prevRecordId, chunkSize);
-            int tmpChunkSize = chunk.size();
-            final long chunkAmount = totalRecordsSize / chunkSize;
-            String progressAnimation = "|/-\\";
-            int animationCounter = 0;
             while (tmpChunkSize > 0) {
                 Long startIndex = chunk.get(tmpChunkSize - 1).getId();
-                executor.submit(new ScrapperTask(flightDao, taskInfoDao, chunk, taskInfo, totalHandledRecords, counter, taskTime));
+                executor.submit(new ScrapperTask(flightDao, taskInfoDao, chunk, null, totalHandledRecords, chunkWithDuplicatesCounter, taskTime));
                 chunk = flightDao.getNextChunk(startIndex, chunkSize);
-                taskInfo = null;
                 tmpChunkSize = chunk.size();
 
                 while (queue.size() == taskQueueSize) {
@@ -175,10 +112,16 @@ public class Transformer {
             System.out.println("Application stopped by timeout. Some tasks stay inProgress. Exit.");
         }
     }
+    private void resumeTasks(ExecutorService executor, List<TaskInfo> currentProcessingState, AtomicInteger chunkWithDuplicatesCounter) {
+        for(TaskInfo taskInfo : currentProcessingState) {
+            List<Flight> chunk = null;
+            if (TransformTaskStatus.TRANSFORM.equals(taskInfo.getTaskStage())) {
+                chunk = flightDao.getNextChunk(taskInfo.getStartIndex(), chunkSize);
+                taskInfo.setTaskStage(TransformTaskStatus.EXTRACT);
+            }
+            executor.submit(new ScrapperTask(flightDao, taskInfoDao, chunk, taskInfo, totalHandledRecords, chunkWithDuplicatesCounter, taskTime));
+        }
 
-
-    public FlightDao getFlightDao() {
-        return flightDao;
     }
 
 }
